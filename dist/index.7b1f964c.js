09bc2d1417848a8bd30b5162597d29304027c80e
jubilyv <110185214+jubilyv@users.noreply.github.com>
2023-12-08 12:11:58 +0530

    test

---
+++ dist/index.7b1f964c.js
@@ -0,0 +1,14 @@
+// nexted
+// ReactElement(object) => HTML (Browser understands)
+const parent = React.createElement("div", {
+    id: "parent"
+}, React.createElement("div", {
+    id: "child"
+}, [
+    React.createElement("h1", {}, "I am an Indian"),
+    React.createElement("h2", {}, "I am not an Indian")
+]));
+const root = ReactDOM.createRoot(document.getElementById("root")); //create root for our react library. put this element into DOM or browser.Root is the place where all the code is run.
+root.render(parent);
+
+//# sourceMappingURL=index.7b1f964c.js.map
